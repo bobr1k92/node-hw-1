4b4eb48ab9ec5469dc42b4492749190575e8ffd7
Denis Mazurov <19bobrik92@gmail.com>
2023-08-02 01:20:22 +0300

    gitignore

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const contacts = require("./contacts");
-const { Command } = require("commander");
+const contacts = require('./contacts');
+const { Command } = require('commander');
 
 const program = new Command();
 program
-  .option("-a, --action <action>", "choose action")
-  .option("-i, --id <id>", "user id")
-  .option("-n, --name <name>", "user name")
-  .option("-e, --email <email>", "user email")
-  .option("-p, --phone <phone>", "user phone");
+  .option('-a, --action <action>', 'choose action')
+  .option('-i, --id <id>', 'user id')
+  .option('-n, --name <name>', 'user name')
+  .option('-e, --email <email>', 'user email')
+  .option('-p, --phone <phone>', 'user phone');
 
 program.parse(process.argv);
 
@@ -15,24 +15,24 @@ const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
+    case 'list':
       const allContacts = await contacts.listContacts();
-      console.table(allContacts);
+      console.log(allContacts);
       break;
-    case "get":
+    case 'get':
       const contact = await contacts.getContactById(id);
-      console.table(contact);
+      console.log(contact);
       break;
-    case "add":
+    case 'add':
       const newContact = await contacts.addContact({ name, email, phone });
-      console.table(newContact);
+      console.log(newContact);
       break;
-    case "remove":
+    case 'remove':
       const deleteContact = await contacts.removeContact(id);
-      console.table(deleteContact);
+      console.log(deleteContact);
       break;
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
